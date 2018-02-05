e3289d9e034a9d6d9dc992b4dba1cf35ca8eddbf
akameco <akameco.t@gmail.com>
2018-02-05 18:34:42 +0900

    chore(npm): add warning

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,18 @@
 import babel from 'rollup-plugin-babel'
 import flow from 'rollup-plugin-flow'
+import nodeResolve from 'rollup-plugin-node-resolve'
 import pkg from './package.json'
 
-const plugins = [flow({ pretty: true }), babel({ exclude: 'node_modules/**' })]
+const plugins = [
+  flow({ pretty: true }),
+  nodeResolve(),
+  babel({ exclude: 'node_modules/**' }),
+]
 
 export default {
   plugins,
-  external: ['react'],
   input: 'src/index.js',
+  external: ['react', 'warning'],
   output: [
     {
       file: pkg.module,
